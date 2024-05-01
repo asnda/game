de514dcba8d717ba88c7da89949ba8999d2952c2
lethabomichelle <lethabomichelle@gmail.com>
2024-05-01 22:46:31 +0200

    changed dice pictures, fixed results display, added game guide button and popup

--- script.js
+++ script.js
@@ -1,30 +1,10 @@
+let popup = document.getElementById("popup");
 
-function rollTheDice(){
-    let diceNum1 = document.querySelector(".img1");
-    let diceNum2 = document.querySelector(".img2");
-
-    diceNum1.setAttribute("src","diceroll.gif")
-    diceNum2.setAttribute("src","diceroll.gif")
-
-    let result = document.querySelector('h1')
-    setTimeout(() => {
-        let randomNumber1 = Math.floor(Math.random()*6)+1;
-        let randomNumber2 = Math.floor(Math.random()*6)+1;
-
-        diceNum1.setAttribute('src','dice'+randomNumber1+'.png');
-        diceNum2.setAttribute('src','dice'+randomNumber2+'.png');
-
-        //determine the winner
-        if (randomNumber1 == randomNumber2){
-            result.innerHTML = "Draw!"
-        }
-        else if(randomNumber1<randomNumber2){
-            result.innerHTML = (player2+" WINS!");
-        }
-        else{
-            result.innerHTML = (player1+" WINS!");
-        }
-
-    },2500);
+// open popup box 
+function openPopup() {
+    popup.classList.add("open-popup");
 }
 
+function closePopup() {
+    popup.classList.remove("open-popup");
+}

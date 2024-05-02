bd6469cf49c0a2a3857cfff989cd50b5f2d070eb
OmolemoModipane <155568348+OmolemoModipane@users.noreply.github.com>
2024-05-02 09:25:47 +0200

    Update game.js

--- game.js
+++ game.js
@@ -40,7 +40,7 @@ function editNames() {
   }
   document.querySelector("p.Player1").innerHTML = player1;
 
-  document.querySelector("p.Player1").innerHTML = player2;
+  document.querySelector("p.Player2").innerHTML = player2;
 }
 
 

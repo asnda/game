7d92edcfa5b60bcb7e9eacafcb37713e6ed1a219
lethabomichelle <lethabomichelle@gmail.com>
2024-05-02 22:02:59 +0200

    added score reading

--- game.js
+++ game.js
@@ -1,19 +1,50 @@
+const player1ScoreElement = document.getElementById("player-1-score");
+const player2ScoreElement = document.getElementById("player-2-score");
+const title = document.getElementById("title").querySelector("h1");
+
+// set rounds and initial scores 
+const MAX_ROUNDS = 3;
+
+let round = 1;
+let player1Score = 0;
+let player2Score = 0;
 
 function rollTheDice(){
+// if round number is greater than MAX_ROUNDS(3) it reset the game 
+    if (round > MAX_ROUNDS) {
+        reset();
+    }
+
     let diceNum1 = document.querySelector(".img1");
     let diceNum2 = document.querySelector(".img2");
 
-    diceNum1.setAttribute("src","diceroll.gif")
-    diceNum2.setAttribute("src","diceroll.gif")
+ // changes picture source to gif 
+    diceNum1.setAttribute("src","./images/diceroll.gif")
+    diceNum2.setAttribute("src","./images/diceroll.gif")
 
-    let result = document.querySelector('h1')
+    // let result = document.querySelector('h1')
     setTimeout(() => {
         let randomNumber1 = Math.floor(Math.random()*6)+1;
         let randomNumber2 = Math.floor(Math.random()*6)+1;
+        player1Score += randomNumber1;
+        player2Score += randomNumber2;
+
+        title.innerHTML = "Rolling...";
 
         diceNum1.setAttribute('src','./images/'+randomNumber1+'.png');
         diceNum2.setAttribute('src','./images/'+randomNumber2+'.png');
 
+        player1ScoreElement.innerHTML = player1Score;
+        player2ScoreElement.innerHTML = player2Score;
+
+        round += 1;
+        if (round > MAX_ROUNDS) {
+            deterMineWinner();
+            return;
+        }
+
+        title.innerHTML = "Play Game";
+    
         //determine the winner
         if (randomNumber1 == randomNumber2){
             result.innerHTML = "Draw!"
@@ -25,27 +56,6 @@ function rollTheDice(){
             result.innerHTML = ("player1 WINS!");
         }
 
-    },2500);
+    },1500);
 }
 
-//Player Names
-
-function editNames() {
-  player1 = prompt("Change Player1 Name");
-  player2 = prompt("Change Player2 Name");
-
-  if (player1.length < 1 || player2.length < 1) {
-    alert("please enter valid name");
-    return;
-  }
-  document.querySelector("p.Player1").innerHTML = player1;
-
-  document.querySelector("p.Player2").innerHTML = player2;
-}
-
-//reset game
-
-document.querySelector(".reset-btn").addEventListener("click", function () {
-  window.location.reload();
-  return false;
-});
--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+let player1Score = document.getElementById('player1score');
+let player2Score = document.getElementById('player2score');
+let result = document.querySelector('h1')
+const playButton = document.getElementById("play-btn");
+
+const maxRounds = 3;
+
+let round = 0;
+let player1 = 0;
+let player2 = 0;
+
 let popup = document.getElementById("popup");
 
 // open popup box 
@@ -7,4 +18,81 @@ function openPopup() {
 
 function closePopup() {
     popup.classList.remove("open-popup");
+}
+
+function rollTheDice(){
+    let diceNum1 = document.querySelector(".img1");
+    let diceNum2 = document.querySelector(".img2");
+
+    diceNum1.setAttribute("src","diceroll.gif")
+    diceNum2.setAttribute("src","diceroll.gif")
+
+    setTimeout(() => {
+        let randomNumber1 = Math.floor(Math.random()*6)+1;
+        let randomNumber2 = Math.floor(Math.random()*6)+1;
+        player1 += randomNumber1;
+        player2 += randomNumber2;
+
+        diceNum1.setAttribute('src','./images/'+randomNumber1+'.png');
+        diceNum2.setAttribute('src','./images/'+randomNumber2+'.png');
+
+        player1Score.innerHTML = player1;
+        player2Score.innerHTML = player2;
+
+        // check gore ke round mang 
+        round += 1;
+        if (round == maxRounds) {
+            if (player1 == player2) {
+                result.innerHTML = "Draw!";
+            } else if (player1 < player2) {
+                result.innerHTML =document.querySelector("p.Player2").innerHTML + " wins!";
+            } else {
+                result.innerHTML = document.querySelector("p.Player1").innerHTML + " wins!";
+            }
+            disablePlayButton();
+            return;
+        }
+
+
+    },2500);
+}
+
+
+
+
+//Player Names
+
+function editNames() {
+  player1Name = prompt("Change Player1 Name");
+  player2Name = prompt("Change Player2 Name");
+
+  if (player1Name.length < 1 || player2Name.length < 1) {
+    alert("please enter valid name");
+    return;
+  }
+  document.querySelector("p.Player1").innerHTML = player1Name;
+
+  document.querySelector("p.Player2").innerHTML = player2Name;
+}
+
+
+function reset() {
+    result.innerHTML = "Play Game";
+    
+
+    round = 0;
+    player1 = 0;
+    player2 = 0;
+    player1Score.innerHTML = player1;
+    player2Score.innerHTML = player2;
+
+    playButton.disabled = false;
+    playButton.style.cursor = "pointer";
+    playButton.style.opacity = 1;
+}
+
+function disablePlayButton() {
+    playButton.disabled = true;
+    playButton.style.cursor = "not-allowed";
+    playButton.style.opacity = 0.1;
 }
